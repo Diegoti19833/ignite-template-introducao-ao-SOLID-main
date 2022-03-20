aeb9c3d8234f2a39e67fc3b392e4124b48dcba73
Diegoti19833 <56735733+Diegoti19833@users.noreply.github.com>
2022-03-20 16:10:46 -0300

    Update CreateUserUseCase.ts

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,8 +10,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User Already Existe");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+    return user;
   }
 }
 
-export { CreateUserUseCase };
+export { CreateUserUseCase }
